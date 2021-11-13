daaf20898d8a611c961b9327f56338a53146bf62
Fledson Henrique <fledsonhenrique@gmail.com>
2021-11-12 23:26:37 -0300

    acionando mensagens para diferentes eventos, refatorando o codigo

--- script.js
+++ script.js
@@ -27,39 +27,75 @@ function validateField(field) {
         return foundError;
     }
 
-    function setCustomMessage(message = "Campo Obrigatório") {
+    // função para devinir o tipo de erro que estou tendo e passar a mensagem correta
+    function customMessage(typeError){
+        const messages = {
+            text: {
+                valueMissing: "Porfavor, preencha este champo"
+            },
+            email: {
+                valueMissing: "Campo Obrigatório",
+                typeMismatch: "Por favor, preencha um email válido"
+            }
+        }
+
+        // pegando o objeto "messages" e pegando chave do campo que tem a mesma propriedade do field (input) 
+        // se for eu passo o tipo do erro[typeError] para ele reconhecermessages[field.type] qual mensagem mandarmessages[field.type][typeError]
+        return messages[field.type][typeError]
+    }
+
+    function setCustomMessage(message) {
+          // abaxo, pego meu input "field" e uso o parentNode para acessar o nó pai "a div.input" e detro procuro o span.error
+        const spanError = field.parentNode.querySelector("span.error")
+
+        if (message) {
+            // se tiver erro ele adiciona ao spanErro e adiciona a classe active e escreve no html o texto "Campo Obrigatório"
+            spanError.classList.add("active")
+            spanError.innerHTML = message
+    
+        } else {
+            // se tiver erro ele adiciona ao spanErro a Remove a classe active e define no html o texto vazio
+            spanError.classList.remove("active")
+            spanError.innerHTML = ""
+    
+        }
 
     }
 
-    return verifyErrors()
+    // VENDO SE O ERRO EXISTE E ACIONANDO O MEU SPAN DO HTML PARA MANIPULAÇÃO
+    return function() {
+        // armazenando erro (se houver) na constante
+        const error = verifyErrors()
+        // se tiver erro 
+        if (error) {
+            // passar a mensagem de erro pra dentro de customMessage
+            const message = customMessage(error)
+            // se estiver errado, adionando uma borda vermelha ao input para sinalizar
+            field.style.borderColor = "red"
+            // se tiver erro ele adiciona ao spanErro e adiciona a classe active e escreve no html o texto "Campo Obrigatório"
+            // passando a mensagem correta aqui dentro da função setCustomMessage
+            setCustomMessage(message)
+
+        } else {
+            // se estiver tudo certo, adionando uma borda verde ao input para sinalizar
+            field.style.borderColor = "green"
+            // se tiver erro ele adiciona ao spanErro a Remove a classe active e define no html o texto vazio
+            setCustomMessage()
+        }
+    }
 }
 
 //função que vai validar os erros
 function customValidation(event) {
 
-
-
     // pegando o evento target (alvo que disparou o evento)
     const field = event.target
 
-    // pegando o erro e armazenando na variavel
-    const error = validateField(field)
+    // instanciando minha função => pegando ela do return
+    const validation = validateField(field)
 
-    // abaxo, pego meu input "field" e uso o parentNode para acessar o nó pai "a div.input" e detro procuro o span.error
-    const spanError = field.parentNode.querySelector("span.error")
-
-    // VENDO SE O ERRO EXISTE E ACIONANDO O MEU SPAN DO HTML PARA MANIPULAÇÃO
-    if (error) {
-        // se tiver erro ele adiciona ao spanErro e adiciona a classe active e escreve no html o texto "Campo Obrigatório"
-        spanError.classList.add("active")
-        spanError.innerHTML = "Campo Obrigatório"
-
-    } else {
-        // se tiver erro ele adiciona ao spanErro a Remove a classe active e define no html o texto vazio
-        spanError.classList.remove("active")
-        spanError.innerHTML = ""
-
-    }
+    // chamando a função
+    validation()
 
     /*
     COMO DESATIVEI O BUBBLE LÁ EM CIMA, ESSA MENSAGEM PERSONALIZADA NÃO IRÁ MAIS APARECE, ESSE BLOCO DE CODIGO PERDE O SENTIDO
